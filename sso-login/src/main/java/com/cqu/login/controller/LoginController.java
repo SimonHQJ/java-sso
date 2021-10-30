35a55995c1a46d852fcefb9d637285d05dcdf1a8
SimonHQJ <78681669+SimonHQJ@users.noreply.github.com>
2021-10-30 21:11:56 +0800

    Update LoginController.java
    登陆机制

--- sso-login/src/main/java/com/cqu/login/controller/LoginController.java
+++ sso-login/src/main/java/com/cqu/login/controller/LoginController.java
@@ -8,6 +8,13 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class LoginController {
+    //模拟账号密码
+    private static Set<User> dbUsers;
+    static {
+        dbUsers = new HashSet<>();
+        dbUsers.add(new User().setId(1).setUsername("admin").setPassword("admin"));
+    }
+
     public String doLogin(User user,HttpSession session){
         String target = (String) session.getAttribute("target");
         //重定向到target地址
